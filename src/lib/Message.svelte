b76edfcbdac2d3625cb6a77049c966054698bbf6
Melon56 <121840186+Melon56@users.noreply.github.com>
2023-02-15 15:59:48 -0800

    Update Message.svelte

--- src/lib/Message.svelte
+++ src/lib/Message.svelte
@@ -12,7 +12,7 @@ onDbRemoved("messages/" + message.timestamp, (snap:any) => {
  <h3 class="name">{message.username}:<span class="timestamp"> at {timestamp}</span></h3>
  <div class="message-subcontainer">
   <p class="message">{message.message}</p>
-  {#if auth.currentUser.uid == "uhk5bOUQ2sTN4ZbTdWHoRSljW2M2"}
+  {#if auth.currentUser.uid == "/*admins who can delete messages(uid)*/"}
    <button class="delete-btn" on:click={() => {removeDb("messages/" + message.timestamp)}}>Delete</button>
   {/if}
  </div>
